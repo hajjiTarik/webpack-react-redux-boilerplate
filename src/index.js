c9088aa30e3eb8a9d6409bb143afab11a9bc6e69
hajji tarek <hajji-tarik@outlook.fr>
2017-07-04 11:55:38 +0100

    fix errors

--- src/index.js
+++ src/index.js
@@ -1,70 +1,16 @@
-/**
- * Ing_UpdatePaymentTransaction.ds
- * To update order payment intrument with the latest ING transaction details
- *
- */
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { configStore } from './store';
 
-/* API includes */
-var ServiceRegistry = require('dw/svc/ServiceRegistry');
-var Transaction = require('dw/system/Transaction');
-var Site = require('dw/system/Site');
-
-function UpdatePaymentInfo(dwOrder, paymentInstrument)
-{
-  var pi = paymentInstrument;
-  var retrieveSvc = ServiceRegistry.get('Ingenico_Retrieve');
-  var params = new Object(); // you must use literal decalaration, ex var params = {} or var Params = new Object();
-  params.paymentID = pi.paymentTransaction.transactionID;
-  var result = retrieveSvc.call(params);
-  if(result.ok)
-  {
-    var approveResp = JSON.parse(result.object.text); // prevent result.object for been null or undefined
-    Transaction.wrap(function () {
-      pi.paymentTransaction.custom.ING_status = approveResp.status;
-      pi.paymentTransaction.custom.ING_authorisationCode = approveResp.paymentOutput.cardPaymentMethodSpecificOutput.authorisationCode;
-      pi.paymentTransaction.custom.ING_statusCategory = approveResp.statusOutput.statusCategory;
-      pi.paymentTransaction.custom.ING_statusCode = approveResp.statusOutput.statusCode;
-      dwOrder.custom.ING_status = approveResp.status;
-      if('isAuthorized' in approveResp.statusOutput)
-      {
-        dwOrder.custom.ING_isAuthorized = approveResp.statusOutput.isAuthorized;
-      }
-      dwOrder.custom.ING_isRefundable = approveResp.statusOutput.isRefundable;
-      dwOrder.custom.ING_isCancellable = approveResp.statusOutput.isCancellable;
-      //save transaction details in order custom attr
-      if(Site.current.getCustomPreferenceValue('ING_enableTransLogs'))
-      {
-        var orderTransactions = dwOrder.custom.ING_TransactionHistory.slice();
-        var authSpecificData = {};
-        authSpecificData.transactionDate = (new Date()).toUTCString();
-        authSpecificData.IngenicoAmount = approveResp.paymentOutput.amountOfMoney.amount;
-        authSpecificData.status = approveResp.status;
-        authSpecificData.statusCode =approveResp.statusOutput.statusCode;
-        authSpecificData.statusCategory = approveResp.statusOutput.statusCategory;
-        authSpecificData.authCode = approveResp.paymentOutput.cardPaymentMethodSpecificOutput.authorisationCode;
-        orderTransactions.push(JSON.stringify(authSpecificData));
-        dwOrder.custom.ING_TransactionHistory = orderTransactions;
-      }
-    });
-
-  }else
-  {
-    if(Site.current.getCustomPreferenceValue('ING_enableTransLogs'))
-    {
-      //save transaction details in order custom attr
-      var orderTransactions = dwOrder.custom.ING_TransactionHistory.slice();
-      Transaction.wrap(function () {
-        var authSpecificData = {};
-        authSpecificData.transactionDate = (new Date()).toUTCString();
-        authSpecificData.errorResult = JSON.parse(result.errorMessage);
-        orderTransactions.push(JSON.stringify(authSpecificData));
-        dwOrder.custom.ING_TransactionHistory = orderTransactions;
-      });
-    }
+const store = configStore();
+export class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+      <h1>Hello am fucking toto 4 </h1>
+    </Provider>
+  );
   }
 }
-
-/*
- * Local methods
- */
-exports.UpdatePaymentInfo = UpdatePaymentInfo;
+ReactDom.render(<App />, document.getElementById('root'));
